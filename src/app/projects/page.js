56e849fd4774451c57318572873433ac0f09973f
Byunghee Kang <byungheekang@g.skku.edu>
2024-07-29 16:02:25 +0900

    root layout 수정

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,10 +1,20 @@
+import Project from "@/components/project";
+import Todo from "@/components/todo";
 
 export default function Home() {
-	let name = 'grass'
-	return (
-	  <div>
-		<h1>Projects Page</h1>
-	  </div>
-	);
-  }
-  
+  const projcets = [1, 2, 3, 4, 5, 6, 7];
+  return (
+    <>
+      <div className="border-4 border-gray-300 w-144 pr-12 overflow-y-scroll no-scrollbar">
+        {projcets.map((n, idx) => (
+          <Project key={idx} name={"test" + n} />
+        ))}
+      </div>
+      <div className="border-4 border-gray-300 w-144 pl-12 overflow-y-scroll no-scrollbar">
+        {projcets.map((n, idx) => (
+          <Todo key={idx} name={"test" + n} />
+        ))}
+      </div>
+    </>
+  );
+}
