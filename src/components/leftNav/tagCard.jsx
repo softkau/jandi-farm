782efd4ae15b872a2e19f370ac3d007a5fccbf7c
Byunghee Kang <byungheekang@g.skku.edu>
2024-08-22 17:16:47 +0900

    태그 우클릭 이벤트 추가

--- src/components/leftNav/tagCard.jsx
+++ src/components/leftNav/tagCard.jsx
@@ -1,17 +1,67 @@
+"use client";
+
+import { useState, useEffect, useRef } from "react";
+
 export default function TagCard({
   name,
   isSelected,
   handleSelected = () => {},
   textSize,
 }) {
+  const [menuPosition, setMenuPosition] = useState({ x: 0, y: 0 });
+  const [showMenu, setShowMenu] = useState(false);
+  const menuRef = useRef(null);
+
+  // 우클릭시 이벤트 발생
+  const handleContextMenu = (e) => {
+    e.preventDefault();
+    setMenuPosition({ x: e.pageX, y: e.pageY });
+    setShowMenu(true);
+  };
+
+  // 화면 아무곳이나 누르거나 esc누르면 누르면 취소
+  const handleClickOutside = (e) => {
+    if (menuRef.current && !menuRef.current.contains(e.target)) {
+      setShowMenu(false);
+    }
+  };
+  const handleEscKey = (e) => {
+    if (e.key === "Escape") {
+      setShowMenu(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    document.addEventListener("keydown", handleEscKey);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+      document.removeEventListener("keydown", handleEscKey);
+    };
+  }, []);
+
   return (
-    <div
-      className={`rounded-lg border-2 border-gray-500 px-2 cursor-pointer ${textSize} ${
-        isSelected && "bg-gray-500"
-      }`}
-      onClick={() => handleSelected(name)}
-    >
-      {name}
+    <div>
+      <div
+        className={`rounded-lg border-2 border-gray-500 px-2 cursor-pointer ${textSize} ${
+          isSelected && "bg-gray-500"
+        }`}
+        onClick={() => handleSelected(name)}
+        onContextMenu={handleContextMenu}
+      >
+        {name}
+      </div>
+      {showMenu && (
+        <ul
+          ref={menuRef}
+          className="absolute bg-white shadow-lg border rounded-md"
+          style={{ top: menuPosition.y, left: menuPosition.x }}
+        >
+          <li className="px-4 py-2 cursor-pointer hover:bg-gray-100">수정</li>
+          <li className="px-4 py-2 cursor-pointer hover:bg-gray-100">삭제</li>
+        </ul>
+      )}
     </div>
   );
 }
