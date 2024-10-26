557697fccd3973719fa39bef553fdc56a2866a31
softkau <paul3143@naver.com>
2024-10-26 10:39:29 +0900

    middleware modification

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
+      <h1 className="font-bold text-4xl">Root 페이지입니다. 잔디농장 HOME을 클릭해서 접속해주세요...</h1>
       <Link href={"/home"}>잔디농장 HOME</Link>
     </div>
   );
