f1d9170f9a83255f20714b7493247be72d48feb0
byunghee <byungheekang@g.skku.edu>
2024-10-30 16:51:35 +0900

    todo 완료된 항목 done으로 처리

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -84,6 +84,7 @@ export default function Home() {
           setTodoList(
             json.map((todo) => {
               return {
+                id: todo._id,
                 title: todo.title,
                 detail: todo.detail,
                 due_date: new Date(todo.due_date),
@@ -104,13 +105,17 @@ export default function Home() {
     }
   }, [session?.user?.id]);
 
+  console.log(todoList);
+
   return (
     <div className="w-full h-screen flex justify-between">
       <div className="w-80 h-full flex-shrink-0 flex flex-col bg-zinc-50">
         <div className="h-20 bg-green-200 flex justify-center items-center">
-          <Sprout size={35}/>
-          <span className="font-extrabold text-3xl">&nbsp;JANDI FARM&nbsp;</span>
-          <Sprout size={35}/>
+          <Sprout size={35} />
+          <span className="font-extrabold text-3xl">
+            &nbsp;JANDI FARM&nbsp;
+          </span>
+          <Sprout size={35} />
         </div>
         <div className="h-full flex flex-col justify-between">
           <ProjectContainer
@@ -134,6 +139,7 @@ export default function Home() {
         <div className="flex h-full justify-center">
           <TodoContainer
             todoList={todoList}
+            setTodoList={setTodoList}
             selectedTags={selectedTags}
             selectedProject={selectedProject}
             className="w-full flex flex-col items-center h-full"
@@ -170,7 +176,9 @@ export default function Home() {
               focusedDate: focusedDate,
             }}
             className="absolute right-1 bottom-1"
-            unmount={() => { setShowNewTodo(false); }}
+            unmount={() => {
+              setShowNewTodo(false);
+            }}
           />
         </CSSTransition>
       </div>
