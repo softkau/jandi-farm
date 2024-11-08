773b808e2952861f1ef89b834b628a4d4ef4e931
softkau <paul3143@naver.com>
2024-11-08 15:31:15 +0900

    connect 업데이트2

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,26 +1,37 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
+let isConnected = false;
 
 export const connectToDB = async () => {
-  if (cached.conn) {
-    console.log('[알림] 기존 MongoDB 연결 재사용');
-    return cached.conn;
+  mongoose.set('strictQuery', true);
+  // mongoose.set('autoIndex', false); <= 배포할 때 성능향상을 위해 이 줄을 활성화할 것
+
+  await mongoose.connect(process.env.MONGODB_URI, {
+    dbName: "jandi_farm",
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true
+  });
+
+  isConnected = true;
+  console.log('[알림] MongoDB에 연결되었습니다.');
+  return;
+
+  if (isConnected) {
+    console.log('[알림] MongoDB에 이미 연결되어 있습니다.');
+    return;
   }
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGODB_URI, {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "jandi_farm",
-    }).then((mongoose) => {
-      console.log('[알림] MongoDB에 연결되었습니다.');
-      return mongoose;
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true
     });
-  }
 
-  cached.conn = await cached.promise;
-  return cached.conn;
-};
+    isConnected = true;
+    console.log('[알림] MongoDB에 연결되었습니다.');
+  } catch (error) {
+    console.log('[에러] MongoDB 연결이 실패하였습니다.');
+    console.log(error);
+  }
+}
